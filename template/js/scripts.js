9fe78c9a4616e27b7dacd6dfea666f4a0bebddba
SheilAPK <karaulov.z@mail.ru>
2023-09-26 11:08:28 +0300

    Section request

--- template/js/scripts.js
+++ template/js/scripts.js
@@ -1,12 +1,12 @@
-/*
 $(document).ready(
     function(){
-        $('.header__burger').click(
-            function (event) {
-                $('div.header__burger').toggleClass('header__burger_close');
-                $('nav.header__menu').toggleClass('header__menu_phone');
-            }
-        );
+        // $('.header__burger').click(
+        //     function (event) {
+        //         $('div.header__burger').toggleClass('header__burger_close');
+        //         $('nav.header__menu').toggleClass('header__menu_phone');
+        //     }
+        // );
+
+        $(".request__input").mask("+7 (999) 999-99-99");
     }
 );
-*/
