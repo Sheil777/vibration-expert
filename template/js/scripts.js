9a080aa6b4eb095ef7b569622ad735decb7ddddb
SheilAPK <karaulov.z@mail.ru>
2023-09-27 15:51:32 +0300

    In work

--- template/js/scripts.js
+++ template/js/scripts.js
@@ -7,8 +7,19 @@ $(document).ready(
         //     }
         // );
 
+        // Маски для номеров телефонов
         $(".request__input").mask("+7 (999) 999-99-99");
         $(".faq-right__phone").mask("+7 (999) 999-99-99");
     }
 );
 
+let feedbackSlider = new Swiper('.feedback__slider',{
+    spaceBetween: 30,
+    slidesPerView: 3,
+    slidesPerGroup: 3,
+
+    navigation: {
+        nextEl: ".feedback__next",
+        prevEl: ".feedback__prev",
+    },
+});
